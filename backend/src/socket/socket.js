b66d58a06074df93126f722d75d70cec94a0a24e
Geoffrey82-maker <geoffreyochi82@gmail.com>
2026-06-30 18:40:43 +0300

    Create chat socket.io

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -1,37 +1,128 @@
 import { Server } from "socket.io";
+import Message from "../models/Message.js";
 
 let io;
+
+const onlineUsers = new Map();
 
 export const initSocket = (server) => {
 
     io = new Server(server, {
-
         cors: {
-
             origin: process.env.FRONTEND_URL,
-
             credentials: true
-
         }
-
     });
 
     io.on("connection", (socket) => {
-
         console.log("User Connected:", socket.id);
 
         socket.on("join", (userId) => {
-
             socket.join(userId);
 
+            onlineUsers.set(
+                userId,
+                socket.id
+            );
+
+            io.emit(
+                "online-users",
+                [...onlineUsers.keys()]
+            );
         });
 
-        socket.on("disconnect", () => {
+        socket.on(
+            "join-chat",
 
-            console.log("Disconnected");
+            roomId => {
+                socket.join(roomId);
+            }
+        );
+        
+        socket.on("send-message", async(data)=>{
 
-        });
+                try{
+                    const message = await Message.create({
+                        room:data.room,
+                        sender:data.sender,
+                        message:data.message
+                    });
 
+                    const populated = await Message.findById(message._id).populate(
+                        "sender",
+                        "firstname lastname role"
+                    );
+
+                    io.to(data.room)
+
+                    .emit(
+                        "receive-message",
+                        populated
+                    );
+                }
+
+                catch(error){
+                    console.error(error);
+                }
+            }
+        );
+
+        socket.on("typing", roomId => {
+                socket.to(roomId)
+                .emit(
+                    "user-typing"
+                );
+            }
+        );
+
+        socket.on("stop-typing", roomId=>{
+
+                socket.to(roomId)
+                .emit(
+                    "user-stop-typing"
+                );
+            }
+        );
+
+        socket.on("message-read", async(messageId)=>{
+                await Message.findByIdAndUpdate(
+                    messageId,
+                    {
+                        read:true
+                    }
+                );
+
+                io.emit(
+                    "message-read",
+                    messageId
+                );
+            }
+        );
+
+        socket.on("disconnect", ()=>{
+
+                for(const [userId,id]
+                    of onlineUsers){
+
+                        if(id===socket.id){
+
+                            onlineUsers.delete(userId);
+                            break;
+
+                        }
+                }
+
+                io.emit(
+
+                    "online-users",
+
+                    [...onlineUsers.keys()]
+
+                );
+
+            }
+
+        );
     });
 
 };
